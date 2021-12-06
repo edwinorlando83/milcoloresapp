05022085647d94a68b3ad7c7565d5318e3e1002c
edwinorlando83 <edwinorlando83@gmail.com>
2021-12-06 14:59:33 +0000

    cambios

---
+++ milcoloresapp/milcoloresapp/doctype/cliente/test_cliente.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, eoch and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCliente(unittest.TestCase):
+	pass
---
+++ milcoloresapp/milcoloresapp/doctype/pedido/pedido.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, eoch and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Pedido', {
+	// refresh: function(frm) {
+
+	// }
+});
