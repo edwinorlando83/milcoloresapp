b6620623b2543cd577b389bb2c2bee066fc9963f
ORLANDO <echolota@sweadenseguros.com>
2022-02-14 06:21:37 -0500

    cambios

--- milcoloresapp/milcoloresapp/filtros/filtros.py
+++ milcoloresapp/milcoloresapp/filtros/filtros.py
@@ -2,14 +2,24 @@ import frappe
 
 def get_permission_query_conditions_cliente(user):
     if not user: 
-        user = frappe.session.user    
+        user = frappe.session.user 
+
+    if 'EMPRESA' in frappe.get_roles(user):
+            return ""
+               
     if 'CLIENTE' in frappe.get_roles(user):
         return """(tabCliente.correo = '{0}')""".format(user)
 
 def get_permission_query_conditions_pedidos(user):
     if not user: 
         user = frappe.session.user 
-
+    
+    if 'System Manager' in frappe.get_roles(user):
+        return ""
+    
+    if 'EMPRESA' in frappe.get_roles(user):
+            return ""
+ 
     cli = frappe.get_doc("Cliente", {"correo":user })
 
     if 'CLIENTE' in frappe.get_roles(user):
